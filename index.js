612e439ec4c53fbe35494ed13f72051600a39113
VH-str <77taxman77@gmail.com>
2024-11-26 17:25:33 +0000

    Deploying to gh-pages from @ VH-str/project-JS-TeamWork-FullStalker@b3822938085e97dbc32505736ed0fb1bf134386a 🚀

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function o(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();const b="data:image/svg+xml,%3csvg%20aria-hidden='true'%20style='position:%20absolute;%20width:%200;%20height:%200;%20overflow:%20hidden;'%20version='1.1'%20xmlns='http://www.w3.org/2000/svg'%20xmlns:xlink='http://www.w3.org/1999/xlink'%3e%3cdefs%3e%3csymbol%20id='icon-icon_visit'%20viewBox='0%200%2032%2032'%3e%3cpath%20fill='none'%20stroke='%2300b068'%20style='stroke:%20var(--color1,%20%2300b068)'%20stroke-linejoin='round'%20stroke-linecap='round'%20stroke-miterlimit='4'%20stroke-width='2.6667'%20d='M6.333%2025.667l19.333-19.333M25.667%206.333h-18M25.667%206.333v18'%3e%3c/path%3e%3c/symbol%3e%3c/defs%3e%3c/svg%3e",p="/project-JS-TeamWork-FullStalker/assets/projects-desk-03-min-bAztutHK.png",d="/project-JS-TeamWork-FullStalker/assets/projects-desk-03@2x-min-D7O6BJsL.png",g="/project-JS-TeamWork-FullStalker/assets/projects-mob-03-min-BsXsyoM6.png",T="/project-JS-TeamWork-FullStalker/assets/projects-mob-03@2x-min-tmcSzpo2.png",F="/project-JS-TeamWork-FullStalker/assets/projects-tabl-03-min-ZgDTbOh9.png",h="/project-JS-TeamWork-FullStalker/assets/projects-tabl-03@2x-min-Dqd8dn0C.png",i="/project-JS-TeamWork-FullStalker/assets/projects-desk-04-min-yxZx66BP.png",J="/project-JS-TeamWork-FullStalker/assets/projects-desk-04@2x-min-DHJNdR6B.png",W="/project-JS-TeamWork-FullStalker/assets/projects-mob-04-min-5yr3aGDi.png",f="/project-JS-TeamWork-FullStalker/assets/projects-mob-04@2x-min-BeXemoj4.png",D="/project-JS-TeamWork-FullStalker/assets/projects-tabl-04-min-Dt9XScdK.png",v="/project-JS-TeamWork-FullStalker/assets/projects-tabl-04@2x-min-B8Ml2TOY.png",m="/project-JS-TeamWork-FullStalker/assets/projects-desk-05-min-CfD1cLSZ.png",_="/project-JS-TeamWork-FullStalker/assets/projects-desk-05@2x-min-BRPn2X1J.png",w="/project-JS-TeamWork-FullStalker/assets/projects-mob-05-min-Db3py1Xm.png",y="/project-JS-TeamWork-FullStalker/assets/projects-mob-05@2x-min-B8zvmCB6.png",M="/project-JS-TeamWork-FullStalker/assets/projects-tabl-05-min-F3tbbktz.png",B="/project-JS-TeamWork-FullStalker/assets/projects-tabl-05@2x-min-BRqBFzgK.png",j="/project-JS-TeamWork-FullStalker/assets/projects-desk-06-min-X_gqtJm5.png",P="/project-JS-TeamWork-FullStalker/assets/projects-desk-06@2x-min-Dj8eCfdF.png",C="/project-JS-TeamWork-FullStalker/assets/projects-mob-06-min-C9QNW9Tj.png",L="/project-JS-TeamWork-FullStalker/assets/projects-mob-06@2x-min-DEtw7lg5.png",O="/project-JS-TeamWork-FullStalker/assets/projects-tabl-06-min-ugbcXMlD.png",X="/project-JS-TeamWork-FullStalker/assets/projects-tabl-06@2x-min-CmmfjQzn.png",k="/project-JS-TeamWork-FullStalker/assets/projects-desk-07-min--lZv1p8n.png",z="/project-JS-TeamWork-FullStalker/assets/projects-desk-07@2x-min-DmSkXiQs.png",N="/project-JS-TeamWork-FullStalker/assets/projects-mob-07-min-BSVWNsA-.png",$="/project-JS-TeamWork-FullStalker/assets/projects-mob-07@2x-min-Dd3dUvIN.png",q="/project-JS-TeamWork-FullStalker/assets/projects-tabl-07-min-CPlUFmPk.png",A="/project-JS-TeamWork-FullStalker/assets/projects-tabl-07@2x-min-BDIDMcu4.png",x="/project-JS-TeamWork-FullStalker/assets/projects-desk-08-min-xocuOah2.png",I="/project-JS-TeamWork-FullStalker/assets/projects-desk-08@2x-min-BIWP6k6v.png",K="/project-JS-TeamWork-FullStalker/assets/projects-mob-08-min-DBYeKoNQ.png",Z="/project-JS-TeamWork-FullStalker/assets/projects-mob-08@2x-min-Ws78KLoX.png",E="/project-JS-TeamWork-FullStalker/assets/projects-tabl-08-min-C_fZqAdW.png",H="/project-JS-TeamWork-FullStalker/assets/projects-tabl-08@2x-min-DRzjhCHA.png",u="/project-JS-TeamWork-FullStalker/assets/projects-desk-09-min-DsvIyQeP.png",R="/project-JS-TeamWork-FullStalker/assets/projects-desk-09@2x-min-BvUdwZmo.png",Q="/project-JS-TeamWork-FullStalker/assets/projects-mob-09-min-DX2Z5Nr9.png",V="/project-JS-TeamWork-FullStalker/assets/projects-mob-09@2x-min-Cc_85EFK.png",U="/project-JS-TeamWork-FullStalker/assets/projects-tabl-09-min-DT00A0X5.png",G="/project-JS-TeamWork-FullStalker/assets/projects-tabl-09@2x-min-BOveHSsW.png",a=[{alt:"Mimino website",img:p,sources:{mobile:{"1x":g,"2x":T},tablet:{"1x":F,"2x":h},desktop:{"1x":p,"2x":d}}},{alt:"Vyshyvanka vibes Landing Page",img:i,sources:{mobile:{"1x":W,"2x":f},tablet:{"1x":D,"2x":v},desktop:{"1x":i,"2x":J}}},{alt:"Power pulse webservice",img:m,sources:{mobile:{"1x":w,"2x":y},tablet:{"1x":M,"2x":B},desktop:{"1x":m,"2x":_}}},{alt:"Chego jewelry website",img:j,sources:{mobile:{"1x":C,"2x":L},tablet:{"1x":O,"2x":X},desktop:{"1x":j,"2x":P}}},{alt:"Energy flow webservice",img:k,sources:{mobile:{"1x":N,"2x":$},tablet:{"1x":q,"2x":A},desktop:{"1x":k,"2x":z}}},{alt:"Starlight studio landing page",img:x,sources:{mobile:{"1x":K,"2x":Z},tablet:{"1x":E,"2x":H},desktop:{"1x":x,"2x":I}}},{alt:"Fruitbox online store",img:u,sources:{mobile:{"1x":Q,"2x":V},tablet:{"1x":U,"2x":G},desktop:{"1x":u,"2x":R}}}],Y=document.querySelector(".project-list"),S=document.querySelector(".more-projects");let ee=document.querySelector("li.project_card");S.addEventListener("click",te);let c=0;function te(){const l=ee.getBoundingClientRect().height,r=a.slice(c,c+3);Y.insertAdjacentHTML("beforeend",oe(r));let o=3;a.length-c<3&&(o=a.length-c),window.scrollBy({top:l*o,left:0,behavior:"smooth"}),c+=r.length,c>=a.length&&S.classList.add("hidden")}function oe(l){return l.map(({alt:s,img:r,sources:{mobile:o,tablet:e,desktop:t}})=>`
+      <li class="project_card">
+        <picture>
+          <source
+            media="(max-width: 767px)"
+            srcset="${o["1x"]} 1x, ${o["2x"]} 2x"
+          />
+          <source
+            media="(max-width: 1279px)"
+            srcset="${e["1x"]} 1x, ${e["2x"]} 2x"
+          />
+          <source
+            media="(min-width: 1280px)"
+            srcset="${t["1x"]} 1x, ${t["2x"]} 2x"
+          />
+          <img
+            class="project-img-card"
+            src="${r}"
+            alt="${s}"
+            loading="lazy"
+          />
+        </picture>
+
+        <p class="project-skills">React, JavaScript, Node JS, Git</p>
+        <div class="bottom-box">
+          <h3 class="project-name">${s}</h3>
+<a
+            class="visit-button"
+            href="https://github.com/VH-str/project-JS-TeamWork-FullStalkers"
+            target="_blank"
+          >
+            <span>VISIT</span>
+            <svg class="button_icon" width="24" height="24">
+              <use xlink:href="${b}#icon-icon_visit"></use>
+            </svg>
+          </a>
+        </div>
+      </li>`).join("")}
 //# sourceMappingURL=index.js.map
